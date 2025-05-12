fa1aae1398c5a290afb25da9b2b71684861a8170
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2025-05-12 15:40:50 +0200

    Update tox.ini && conf.py file (#42)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- doc/best-practice/source/conf.py
+++ doc/best-practice/source/conf.py
@@ -38,7 +38,9 @@ otcdocs_doc_type = 'best-practice'
 otcdocs_service_category = 'security-services'
 otcdocs_service_title = 'Dedicated Web Application Firewall'
 otcdocs_service_type = 'wafd'
+otcdocs_cloud_environment = 'eu_de'
 otcdocs_search_environment = 'hc_de'
+otcdocs_search_index = 'search_index_de'
 otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
